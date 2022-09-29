5a4b5eba78a2f496549dc36cf28dc47b43a3459a
Samar Fatima <samar.khan7393@gmail.com>
2022-09-29 06:53:27 -0700

    removed unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./App.css";
+
 import NewExpense from "./Components/NewExpense/NewExpense";
 import Expenses from "./Components/Expenses/Expenses";
 
